f1cdd7b5689d23668f0ed610dbaf8129f222c4f1
jamescross813 <crossjames813@gmail.com>
2021-10-18 21:04:03 -0500

    done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
 export function manageFriends(state, action){
+    // console.log(action)
+    switch(action.type){
+        case "ADD_FRIEND":
+            return {
+                ...state,
+                friends: [
+                    ...state.friends,
+                    {
+                        ...action.friend
+                    }
+                ]
+            }
+            ;
+        case "REMOVE_FRIEND":
+            return{friends: state.friends.filter(friend => friend.id !== action.id)};
+        default:
+            return state
+    }
 }
+
+
+
+// [...array.slice(0, action.index), ...array.slice(action.index + 1)]
